43bd7b811f56a6a88e86422ae85333600de6c486
oheasy1990 <oheasy1990@foxmail.com>
2015-09-01 11:51:35 +0800

    foreach where when set

--- mavenMybatis/src/main/java/com/hxy/mybatis/MainApp.java
+++ mavenMybatis/src/main/java/com/hxy/mybatis/MainApp.java
@@ -6,7 +6,10 @@ import com.hxy.mybatis.util.MyBatisUtil;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by hxy-pc on 8/23/2015.
@@ -27,7 +30,7 @@ public class MainApp {
 //        for (User user : users) {
 //            System.out.println(user.getUserAddress());
 //        }
-
+//
 //        //insert
 //        mainApp.insertUser(new User("dd",23,"nanjing"));
 
@@ -37,9 +40,41 @@ public class MainApp {
 //        //update
 //        mainApp.updateUser(new User("xx",33,"DD"));
 
-        //select if
-        User user = mainApp.selectIf(new User());
-        System.out.println(user.getUserAddress());
+//        //select if
+//        User user = mainApp.selectIf(new User());
+//        System.out.println(user.getUserAddress());
+
+//        //select choose
+//        User user = mainApp.selectChoose(new User(null, 0, "nanjing"));
+//        System.out.println(user.getUserName());
+
+//        //select where
+//        User user = mainApp.selectWhere(new User("dd", 33, "n"));
+//        System.out.println(user.getUserAddress());
+
+//        //select trim
+//        User user = mainApp.selectTrim(new User("xx", 22, "E"));
+//        System.out.println(user.getUserAddress());
+
+//        //update set
+//        mainApp.updateSet(new User("xx",100,"dd"));
+
+//        //update list
+//        List<User> users = new ArrayList<User>();
+//        users.add(new User("dd", 34, "Dd"));
+//        users.add(new User("xx", 34, "xx"));
+//        mainApp.updateList(users);
+
+//        //update array
+//        int[] ids = {1, 2, 3, 4};
+//        mainApp.updateArray(ids);
+
+        //update map
+        Map<String, Object> map = new HashMap<String, Object>();
+        int[] ids = {1, 2};
+        map.put("ids", ids);
+        map.put("name", "hxy");
+        mainApp.updateMap(map);
     }
 
     private User getUserById(int id) {
@@ -130,5 +165,107 @@ public class MainApp {
         }
 
         return userSelect;
+    }
+
+    private User selectChoose(User user) {
+        SqlSession session = null;
+        User userChoose = null;
+        try {
+            session = factory.openSession();
+            UserMapper mapper = session.getMapper(UserMapper.class);
+            userChoose = mapper.selectChoose(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return userChoose;
+    }
+
+    private User selectWhere(User user) {
+        SqlSession session = null;
+        User userWhere = null;
+        try {
+            session = factory.openSession();
+            UserMapper mapper = session.getMapper(UserMapper.class);
+            userWhere = mapper.selectWhere(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return userWhere;
+    }
+
+    private User selectTrim(User user) {
+        SqlSession session = null;
+        User userTrim = null;
+        try {
+            session = factory.openSession();
+            UserMapper mapper = session.getMapper(UserMapper.class);
+            userTrim = mapper.selectTrim(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            session.close();
+        }
+        return userTrim;
+    }
+
+    private void updateSet(User user) {
+        SqlSession session = null;
+        try {
+            session = factory.openSession();
+            UserMapper mapper = session.getMapper(UserMapper.class);
+            mapper.updateSet(user);
+            session.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+
+    }
+
+    private void updateList(List<User> users) {
+        SqlSession session = null;
+        try {
+            session = factory.openSession();
+            UserMapper mapper = session.getMapper(UserMapper.class);
+            mapper.updateList(users);
+            session.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    private void updateArray(int ids[]) {
+        SqlSession session = null;
+        try {
+            session = factory.openSession();
+            UserMapper mapper = session.getMapper(UserMapper.class);
+            mapper.updateArray(ids);
+            session.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    private void updateMap(Map map) {
+        SqlSession session = null;
+        try {
+            session = factory.openSession();
+            UserMapper mapper = session.getMapper(UserMapper.class);
+            mapper.updateMap(map);
+            session.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
     }
 }
--- mavenMybatis/src/main/java/com/hxy/mybatis/mapper/UserMapper.java
+++ mavenMybatis/src/main/java/com/hxy/mybatis/mapper/UserMapper.java
@@ -3,6 +3,7 @@ package com.hxy.mybatis.mapper;
 import com.hxy.mybatis.model.User;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by hxy-pc on 8/23/2015.
@@ -19,4 +20,18 @@ public interface UserMapper {
     void updateUser(User user);
 
     User selectIf(User user);
+
+    User selectChoose(User user);
+
+    User selectWhere(User user);
+
+    User selectTrim(User user);
+
+    void updateSet(User user);
+
+    void updateList(List<User> users);
+
+    void updateArray(int id[]);
+
+    void updateMap(Map map);
 }
